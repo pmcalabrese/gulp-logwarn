dd1f05a02a09d007e5cd3704f946921bd116c40d
Pachito Marco Calabrese <pm.calabrese@gmail.com>
2014-06-25 14:34:17 +0200

    clean up

--- index.js
+++ index.js
@@ -11,37 +11,43 @@ module.exports = function(subString) {
    * @param {String} subString    Required. The string to search for;
    * @param {Boolean} allowOverlapping    Optional. Default: false;
    */
-  function occurrences(file, subString2, allowOverlapping){
+  function occurrences(file){
 
       // file.contents = new Buffer(String(file.contents).toString());
-      var string = file.contents.toString();
-      var message = "";
-      var m = 0;
-
-      for (var i = substrings.length - 1; i >= 0; i--) {
-        if(substrings[i].length<=0) return string.length+1;
-        var n=0, pos=0;
-        var step=(allowOverlapping)?(1):(substrings[i].length);
-        while(true){
-            pos=string.indexOf(substrings[i],pos);
-            if(pos>=0){ n++; pos+=step; } else break;
-        }
-        m = m + n;
-        if (n > 0) {
-          message =  message + "    "+ n + " " + "'" + substrings[i] + "'" + ",\n";
-        };
-      };
-      var finalMessage = file.path+" ("+m+")\n" + message;
-      if (m === 0) {
-        console.log(finalMessage.green);
-      } else {
-        console.log(finalMessage.red);
+      if (file.isNull()) {
+         // Do nothing if no contents
       }
-      message = "";
+      
+      if (file.isBuffer()) {
+        var string = file.contents.toString();
+        var message = "";
+        var m = 0;
 
-      return(n);
+        for (var i = substrings.length - 1; i >= 0; i--) {
+          if(substrings[i].length<=0) return string.length+1;
+          var n=0, pos=0;
+          var step=substrings[i].length;
+          while(true){
+              pos=string.indexOf(substrings[i],pos);
+              if(pos>=0){ n++; pos+=step; } else break;
+          }
+          m = m + n;
+          if (n > 0) {
+            message =  message + "    "+ n + " " + "'" + substrings[i] + "'" + ",\n";
+          };
+        };
+        var finalMessage = file.path+" ("+m+")\n" + message;
+        if (m === 0) {
+          console.log(finalMessage.green);
+        } else {
+          console.log(finalMessage.red);
+        }
+        message = "";
+
+        return n;
+      }      
+      
   }
-
 
   return es.map(occurrences);
 };
