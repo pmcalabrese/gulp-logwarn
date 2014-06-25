f110dcbb6f339601a0f33e7654ad0b76ba7c0886
pmcalabrese <pm.calabrese@gmail.com>
2014-06-25 10:29:07 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+gulp-logwarn
+============
+
+A configurable gulp plugin that warn you if you left debug code
---
+++ index.js
@@ -0,0 +1,46 @@
+var es = require('event-stream');
+var colors = require('colors');
+
+module.exports = function(subString) {
+
+  var defaultSubstrings = ["console","$log"];
+  var substrings = defaultSubstrings.concat(subString)
+
+  /** Function count the occurrences of substring in a string;
+   * @param {String} string   Required. The string;
+   * @param {String} subString    Required. The string to search for;
+   * @param {Boolean} allowOverlapping    Optional. Default: false;
+   */
+  function occurrences(file){
+
+      // file.contents = new Buffer(String(file.contents).toString());
+      var string = file.contents.toString();
+      var message = "";
+      var m = 0;
+
+      for (var i = substrings.length - 1; i >= 0; i--) {
+        if(substrings[i].length<=0) return string.length+1;
+        var n=0, pos=0;
+        while(true){
+            pos=string.indexOf(substrings[i],pos);
+            if(pos>=0){ n++; pos+=step; } else break;
+        }
+        m = m + n;
+        if (n > 0) {
+          message =  message + "    "+ n + " " + "'" + substrings[i] + "'" + ",\n";
+        };
+      };
+      var finalMessage = file.path+" ("+m+")\n" + message;
+      if (m === 0) {
+        console.log(finalMessage.green);
+      } else {
+        console.log(finalMessage.red);
+      }
+      message = "";
+
+      return(n);
+  }
+
+
+  return es.map(occurrences);
+};
